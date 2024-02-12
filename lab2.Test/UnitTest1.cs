85ef5fbf3264c1ec77c343f31fc74b92579c71f4
NastyaSh6267 <159644368+NastyaSh6267@users.noreply.github.com>
2024-02-12 13:32:42 +0300

    Create UnitTest1.cs

---
+++ lab2.Test/UnitTest1.cs
@@ -0,0 +1,26 @@
+namespace Slab2.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void AddTrackTest()
+    {
+        var tracks = new List<MusicTrack>();
+        var appController = new AppController(tracks, new AppView());
+        appController.Add(new MusicTrack { Author = "test", Name = "test" });
+        Assert.NotEmpty(tracks);
+
+    }
+    
+    [Theory]
+    [InlineData("dora", "doradura", "kid cudi", "enter galactic")]
+    public void SearchByNameTest(string author1, string name1, string author2, string name2)
+    {
+        var tracks = new List<MusicTrack>();
+        var appController = new AppController(tracks, new AppView());
+        appController.Add(new MusicTrack { Author = author1, Name = name1 });
+        appController.Add(new MusicTrack { Author = author2, Name = name2 });
+        Assert.Equal(appController.Search(author1, 2)[0], tracks[0]);
+    }
+    
+}
